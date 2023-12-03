7d13138eb3f6b958ffc9807e317221b084c4c714
Tyler Lin <tylerhlin@gmail.com>
2023-12-03 18:17:47 -0500

    fix api

--- api/kinexon.js
+++ api/kinexon.js
@@ -4,7 +4,7 @@ const dateFormatter = Intl.DateTimeFormat('sv-SE')
 
 const phases = async (startDate = '2021-09-01', endDate = dateFormatter.format(new Date())) => {
     const response = await fetch(
-        `https://georgia-tech-mccamish.access.kinexon.com/public/v1/statistics/players/in-team-3/phases?apiKey=${API_KEY}&min=${startDate}%2000%3A00%3A00&max=${endDate}%2023%3A59%3A59&fields=`,
+        `https://georgia-tech-mccamish.access.kinexon.com/public/v1/statistics/players/in-team-3/phases?apiKey=${API_KEY}&min=${startDate}&max=${endDate}&fields=`,
         {
             method: 'GET',
             headers: {
